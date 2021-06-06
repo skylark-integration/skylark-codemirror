dcd214014298babbb6eba61108cf2b20786348a5
hudaokeji <github@hudaokeji.com>
2021-06-06 20:17:36 +0900

    update

--- dist/skylark-codemirror/primitives/line/saw_special_spans.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-codemirror - A version of codemirror 5.45  that ported to running on skylarkjs
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-integration/skylark-codemirror/
- * @license MIT
- */
-define(function(){"use strict";let exports={sawReadOnlySpans:!1,sawCollapsedSpans:!1,seeReadOnlySpans:function(){exports.sawReadOnlySpans=!0},seeCollapsedSpans:function(){exports.sawCollapsedSpans=!0}};return exports});
-//# sourceMappingURL=../../sourcemaps/primitives/line/saw_special_spans.js.map
